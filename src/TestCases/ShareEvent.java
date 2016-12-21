de0a4875c5bc37a26892cbd9bc01bef1ee9c6dce
Dzanan <dzanang@gmail.com>
2016-12-21 21:23:02 +0100

    removed unnecessary dependencies

--- src/TestCases/ShareEvent.java
+++ src/TestCases/ShareEvent.java
@@ -11,7 +11,6 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.interactions.Actions;
 
 public class ShareEvent {
 	private WebDriver driver;
